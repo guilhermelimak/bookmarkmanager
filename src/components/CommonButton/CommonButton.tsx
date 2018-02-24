fc7db0c72a50638d22ef317c3804f3e63cecf1cf
Guilherme Lima <guilhermelimak@gmail.com>
2018-02-24 04:39:50 -0300

    feat: Add CommonButton updates

--- src/components/CommonButton/CommonButton.tsx
+++ src/components/CommonButton/CommonButton.tsx
@@ -1,16 +1,37 @@
-import Vue from 'vue'
+import VueComponent from '@/vue-component'
 import { Component, Prop, Emit } from 'vue-property-decorator'
 import { Button } from './style'
+import { ColorNames } from '@/styles/colors'
+
+export interface CommonButtonProps {
+  text: String
+  color?: ColorNames
+  hoverColor?: ColorNames
+}
 
 @Component
-class CommonButton extends Vue {
+class CommonButton extends VueComponent<CommonButtonProps> {
   @Prop() text: String
+
+  @Prop({ default: ColorNames.PRIMARY })
+  color: ColorNames
+
+  @Prop({ default: ColorNames.PRIMARY_DARK })
+  hoverColor: ColorNames
 
   @Emit('click')
   onButtonClick() {}
 
   render(h: Function) {
-    return <Button nativeOnClick={this.onButtonClick}>{this.text}</Button>
+    return (
+      <Button
+        color={this.color}
+        hoverColor={this.hoverColor}
+        nativeOnClick={this.onButtonClick}
+      >
+        {this.text}
+      </Button>
+    )
   }
 }
 
